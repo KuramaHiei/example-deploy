466572157127bf474f37a15009c29d295b5da7c2
KuramaHiei <149123713+KuramaHiei@users.noreply.github.com>
2023-10-26 22:05:30 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/cep">Busca Endereco pelo CEP</RouterLink>
+        <RouterLink to="/cep">Search for the Zip code </RouterLink>
       </nav>
     </div>
   </header>
